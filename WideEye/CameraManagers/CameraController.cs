777f2409e14b32020bc37cf36a51ef0dd21d0334
HL2H0 <gmhisham7@gmail.com>
2026-07-15 09:34:49 +0300

    Refactored `UpdateView` to improve readability

--- WideEye/CameraManagers/CameraController.cs
+++ WideEye/CameraManagers/CameraController.cs
@@ -8,55 +8,76 @@ namespace WideEye.CameraManagers;
 
 public static class CameraController
 {
+    public static ModEnums.ViewMode ActiveViewMode;
+    
     public static void UpdateView(ModEnums.ViewMode viewMode, bool syncElements = false)
     {
         switch (viewMode)
         {
             case ModEnums.ViewMode.Head:
-                Mod.ScSmootherComponent.targetTransform = Mod.StTransform;
-                if(TimelineHelper.UsingTimeline) TimelineHelper.TimelineSmoothFollower.targetTransform = Mod.StTransform;
-                if (HandheldCameraManager.Found) HandheldCameraManager.ActiveScript.SyncCamera = null;
-                SettingsUpdater.UpdateFOV(ModMenu.FOVSlider.Value);
-                FreeCamManager.FreeCamObject.SetActive(false);
+                SetViewHead();
                 break;
-            
             case ModEnums.ViewMode.Handheld:
-                HandheldCameraManager.FindHandheldCamera();
-                if (!HandheldCameraManager.Found) 
-                {
-                    Notifier.Send(new Notification
-                    {
-                        Title = "WideEye | Error",
-                        Message = "Handheld camera not spawned\nPlease spawn the camera first",
-                        Type = NotificationType.Error,
-                        PopupLength = 3,
-                        ShowTitleOnPopup = true
-                    });
-                }
-                
-                if (TimelineHelper.UsingTimeline)
-                {
-                    TimelineHelper.TimelineSmoothFollower.targetTransform = HandheldCameraManager.ActiveScript.cameraTarget;
-                    HandheldCameraManager.ActiveScript.SyncCamera = TimelineHelper.TimelineCamera.gameObject.active
-                        ? TimelineHelper.TimelineCamera
-                        : Mod.ScCameraComponent;
-                }
-                Mod.ScSmootherComponent.targetTransform = HandheldCameraManager.ActiveScript.cameraTarget;
-                SettingsUpdater.UpdateFOV(HandheldCameraManager.ActiveScript.FOV);
-                FreeCamManager.FreeCamObject.SetActive(false);
+                SetViewHandheld();
                 break;
-            
             case ModEnums.ViewMode.FreeCam:
-                Mod.ScSmootherComponent.targetTransform = FreeCamManager.FreeCamObject.transform;
-                if (HandheldCameraManager.Found) HandheldCameraManager.ActiveScript.SyncCamera = null;
-                FreeCamManager.FreeCamObject.transform.position = Player.Head.position;
-                FreeCamManager.FreeCamObject.transform.rotation = Player.Head.rotation;
-                FreeCamManager.FreeCamObject.SetActive(true);
+                SetViewFreeCam();
                 break;
         }
 
         if (syncElements)
             ModMenu.ViewMode.Value = viewMode;
+    }
+
+    private static void SetViewHead()
+    {
+        Mod.ScSmootherComponent.targetTransform = Mod.StTransform;
+        if(TimelineHelper.UsingTimeline) TimelineHelper.TimelineSmoothFollower.targetTransform = Mod.StTransform;
+        if (HandheldCameraManager.Found) HandheldCameraManager.ActiveScript.SyncCamera = null;
+        SettingsUpdater.UpdateFOV(ModMenu.FOVSlider.Value);
+        FreeCamManager.FreeCamObject.SetActive(false);
+        
+        ActiveViewMode = ModEnums.ViewMode.Head;
+    }
+
+    private static void SetViewHandheld()
+    {
+        HandheldCameraManager.FindHandheldCamera();
+        if (!HandheldCameraManager.Found) 
+        {
+            Notifier.Send(new Notification
+            {
+                Title = "WideEye | Error",
+                Message = "Handheld camera not spawned\nPlease spawn the camera first",
+                Type = NotificationType.Error,
+                PopupLength = 3,
+                ShowTitleOnPopup = true
+            });
+        }
+                
+        if (TimelineHelper.UsingTimeline)
+        {
+            TimelineHelper.TimelineSmoothFollower.targetTransform = HandheldCameraManager.ActiveScript.cameraTarget;
+            HandheldCameraManager.ActiveScript.SyncCamera = TimelineHelper.TimelineCamera.gameObject.active
+                ? TimelineHelper.TimelineCamera
+                : Mod.ScCameraComponent;
+        }
+        Mod.ScSmootherComponent.targetTransform = HandheldCameraManager.ActiveScript.cameraTarget;
+        SettingsUpdater.UpdateFOV(HandheldCameraManager.ActiveScript.FOV);
+        FreeCamManager.FreeCamObject.SetActive(false);
+        
+        ActiveViewMode = ModEnums.ViewMode.Handheld;
+    }
+    
+    private static void SetViewFreeCam()
+    {
+        Mod.ScSmootherComponent.targetTransform = FreeCamManager.FreeCamObject.transform;
+        if (HandheldCameraManager.Found) HandheldCameraManager.ActiveScript.SyncCamera = null;
+        FreeCamManager.FreeCamObject.transform.position = Player.Head.position;
+        FreeCamManager.FreeCamObject.transform.rotation = Player.Head.rotation;
+        FreeCamManager.FreeCamObject.SetActive(true);
+        
+        ActiveViewMode = ModEnums.ViewMode.FreeCam;
     }
 
     public static void UpdateAudioSource(ModEnums.AudioSource audioSource, bool syncElements = false)
